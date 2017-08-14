05ae8d86ad3390057262c48d7d51f63ef8586fc7
Thiago Ferreira <thiago.ferreira@fcamara.com.br>
2017-08-14 15:30:30 -0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "Removing image"
 sudo docker rmi -f ng2_image
 
 echo "Create image"
-sudo docker build -t ng2_image
+sudo docker build -t ng2_image .
 
 echo "Create container and start"
 sudo docker run -d -p 80:80 --name ng2_build ng2_image
